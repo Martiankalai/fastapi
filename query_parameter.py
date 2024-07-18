1117dbd70d9236185ebee27ce6ac15055b36d600
Martiankalai <kalaiarasan6923@gmail.com>
2024-07-18 14:25:10 +0530

    second commit

---
+++ query_parameter.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI  # class-FastAPI
+
+app = FastAPI()
+
+new_dict = {
+    "key1": "v1",
+    "key2": "v2",
+    1: "v3",
+    5: "this"
+}
+
+
+@app.get("/")
+async def root():
+    return new_dict
+
+
+@app.get("/sub")
+# query can be used to give and change value dynamically
+async def subpage(sub_page: int | None, query: int | None = None):
+    return new_dict.get(sub_page, query)
