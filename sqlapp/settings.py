c120691e7391797f8ccd77033c1d6821d3735bce
kalaiarasan sivakumar <94048810+Martiankalai@users.noreply.github.com>
2024-07-19 12:16:30 +0530

    Delete sqlapp directory

--- sqlapp/settings.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-sqlalchemy_database_url = "sqlite:///./sqlite.db"
-engine = create_engine(sqlalchemy_database_url, connect_args={
-                       "check_same_thread": False})
-session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
